9f4733b57a6b05eb9ff4f45fe3d08d4b71993286
jisoleil <janggh1012@naver.com>
2023-12-11 14:56:05 +0900

    ts form -usestate, onChange, onSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-
-import Circle from './Circle';
+import { useState } from "react";
 
 
 function App() {
+  const[value, setValue]=useState('')
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget:{value},
+    }=event;
+    setValue(value);
+  };
+  const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log("hello",value);
+  };
   return (
     <div>
-      <Circle borderColor='black' bgColor='teal'/>
-      <Circle bgColor='tomato'text='존나 힘들다.'/>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
